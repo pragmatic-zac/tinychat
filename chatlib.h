8c19fc8d968b3a6c8f8ed00c9d13d15b1d448f50
Zac Jones <zacjones91@gmail.com>
2023-12-16 09:15:56 -0600

    Add library functions for networking and alloc

--- chatlib.h
+++ chatlib.h
@@ -5,6 +5,14 @@
 #ifndef TINYCHAT_CHATLIB_H
 #define TINYCHAT_CHATLIB_H
 
-#endif //TINYCHAT_CHATLIB_H
-
+/* Networking. */
+int createTCPServer(int port);
 int socketSetNonBlockNoDelay(int fd);
+int acceptClient(int server_socket);
+int TCPConnect(char *addr, int port, int nonblock);
+
+/* Allocation. */
+void *chatMalloc(size_t size);
+void *chatRealloc(void *ptr, size_t size);
+
+#endif //TINYCHAT_CHATLIB_H
